ad484df6da92b56784200b5da222509d67057f54
Elliot Mitchum <elliotmitchum@gmail.com>
2018-09-06 09:08:22 +1000

    Add auth server

--- srv/auth.js
+++ srv/auth.js
@@ -1,14 +1,16 @@
 const spotifyApi = require('spotify-web-api-node');
-const scopes = ["playlist-modify-public"]
 
-const credentials = {
-  clientId : process.env.CLIENT_ID,
-  clientSecret : process.env.CLIENT_SECRET,
-  redirectUri : process.env.CALLBACK
+const auth = () => {
+  const scopes = ["playlist-modify-public", "playlist-modify-private"]
+  const credentials = {
+    clientId : process.env.CLIENT_ID,
+    clientSecret : process.env.CLIENT_SECRET,
+    redirectUri : process.env.CALLBACK
+  }
+
+  const spotify = new spotifyApi(credentials)
+  const auth = spotify.createAuthorizeURL(scopes, scopes)
+  return auth
 }
 
-const spotify = new spotifyApi(credentials)
-
-const auth = spotify.createAuthorizeURL(scopes, "")
-
-console.log(auth)
+module.exports = { auth }
